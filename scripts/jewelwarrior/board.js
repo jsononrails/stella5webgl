158f4301635f8307d342bcaf6103bef9ccd7066c
Jason McBride <jsononrails@gmail.com>
2014-01-07 18:12:08 -0500

    board setup

--- scripts/jewelwarrior/board.js
+++ scripts/jewelwarrior/board.js
@@ -28,13 +28,32 @@ jewel.board = (function() {
 	}
 	
 	function fillBoard() {
-		var x, y;
+		var x, y,
+			type;
+			
 		jewels = [];
 		for(x = 0; x < cols; x++) {
 			jewels[x] = [];
 			for(y = 0; y < rows; y++) {
-				jewels[x][y] = randomJewel();
+				type = randomJewel();
+				while((type===getJewel(x-1, y) &&
+					   type===getJewel(x-2, y) &&
+					   type===getJewel(x, y-1) &&
+					   type===getJewel(x, y-2))) {
+						
+						type = randomJewel();
+					}
+					
+					jewels[x][y] = type;
 			}
+		}
+	}
+	
+	function getJewel(x, y) {
+		if(x<0 || x > cols -1 || y < 0 || y > rows-1) {
+			return -1;
+		} else {
+			return jewels[x][y];
 		}
 	}
 	
