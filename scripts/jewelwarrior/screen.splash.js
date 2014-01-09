163e94acbd5e1d88ba5c3916487804198e0fddf2
Jason McBride <jsononrails@gmail.com>
2014-01-08 20:00:21 -0500

    finished progress bar and added background

--- scripts/jewelwarrior/screen.splash.js
+++ scripts/jewelwarrior/screen.splash.js
@@ -1,17 +1,32 @@
 jewel.screens["splash-screen"] = (function(){
 	var game = jewel.game,
 	dom = jewel.dom,
+	$ = dom.$,
 	firstRun = true;
 	
-	function setup() {
-		dom.bind("#splash-screen", "click", function() {
-			game.showScreen("main-menu");
-		})
+	function setup(getLoadProgress) {
+
+		var src = $("#splash-screen")[0];
+		
+		function checkProgress() {
+			var p = getLoadProgress() * 100;
+			
+			$(".indicator", src)[0].style.width = p + "%";
+			
+			if(p==100) {
+				dom.bind("#splash-screen", "click", function() {
+					game.showScreen("main-menu");
+				})
+			} else {
+				setTimeout(checkProgress, 30);
+			}
+		}
+		checkProgress();
 	}
 	
-	function run() {
+	function run(getLoadProgress) {
 		if(firstRun) {
-			setup();
+			setup(getLoadProgress);
 			firstRun = false;
 		}
 	}
