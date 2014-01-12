7cf42b9378acad79dd491424a779640d14e25f39
Jason McBride <jsononrails@gmail.com>
2014-01-12 17:56:57 -0500

    added math library

--- scripts/jewelwarrior/webgl.js
+++ scripts/jewelwarrior/webgl.js
@@ -26,8 +26,60 @@ jewel.webgl = (function() {
 		return program;
 	}
 	
+	function createFloatBuffer(gl, data) {
+		var buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+		
+		return buffer;
+	}
+	
+	function createIndexBuffer(gl, data) {
+		var buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+		
+		gl.bufferData(
+			gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data), gl.STATIC_DRAW
+		);
+		
+		return buffer;
+	}
+	
+	function setModelView(gl, prgm, pos, rot, axis) {
+		var mvMatrix = mat4.identity(mat4.create());
+		
+		mat4.translate(mvMatrix, pos);
+		mat4.rotate(mvMatrix, rot, axis);
+		
+		gl.uniformMatrix4fv(
+			gl.getUniformLocation(prgm, "uModelView"),
+			false, mvMatrix
+		);
+		
+		return mvMatrix;
+	}
+	
+	function setProjection(gl, prgm, fov, aspect, near, far) {
+		var projMatrix = mat4.create();
+		
+		mat4.perspective(
+			fov, aspect,
+			near, far,
+			projMatrix
+		);
+		
+		gl.uniformMatrix4fv(
+			gl.getUniformLocation(prgm, "uProjection"),
+			false, projMatrix
+		);
+		
+		return projMatrix;
+	}
+	
 	return {
 		createShaderObject: createShaderObject,
-		createProgramObject: createProgramObject
+		createProgramObject: createProgramObject,
+		createFloatBuffer: createFloatBuffer,
+		createIndexBuffer: createIndexBuffer
 	};
 })();
